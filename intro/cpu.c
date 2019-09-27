fdd05a1faca9930090b7e0a1fd4e7c7ef37697bd
sinan <seenone@gmail.com>
2019-09-27 09:45:01 +0300

    changed to sleep

--- intro/cpu.c
+++ intro/cpu.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "common.h"
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 
     while (1) {
 	printf("%s\n", str);
-	Spin(1);
+	sleep(1);
     }
     return 0;
 }
